212208e416396fa0af608a97e5b7f5eea6e0fe2a
R. A. Souza <alves.wm@gmail.com>
2020-08-07 19:00:23 -0300

    implements legacy dashboard consumer

--- clients/payout_dashboard/server.js
+++ clients/payout_dashboard/server.js
@@ -1,6 +1,6 @@
 import { connect } from "https://deno.land/x/cotton@v0.6.3/mod.ts";
 import { Application } from "https://deno.land/x/abc@v1.0.2/mod.ts";
-import { reduce } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+import { reduce, merge } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
 
 const app = new Application();
 
@@ -10,15 +10,29 @@ const payouts = async () => {
     database: "../../db/payouts.db",
   });
 
+  let images = [
+    { image: "01" },
+    { image: "02" },
+    { image: "15" },
+    { image: "21" },
+    { image: "26" },
+    { image: "31" },
+    { image: "36" },
+    { image: "46" },
+    { image: "48" },
+    { image: "59" },
+  ];
+
   const payouts = await db.query("SELECT merchant_id, SUM(amount) AS amount FROM payouts GROUP BY merchant_id");
   console.log(payouts)
-  return reduce(payouts, (list, item) => {
-    return {...list, [item.MERCHANT_ID]: item.amount}
-  }, {});
+  return merge(payouts,images)
+  // return reduce(payouts, (list, item) => {
+  //   return {...list, [item.MERCHANT_ID]: item.amount}
+  // }, {});
 }
 
 app
-  .get("/", () => payouts())
+  .get("/api/payouts", () => payouts())
   .start({ port: 8080 });
 
 console.log(`server listening on http://localhost:8080`);
---
+++ clients/payout_dashboard/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import "semantic-ui-css/semantic.min.css";
+import { Card, Icon, Image } from "semantic-ui-react";
+
+function App() {
+  const [payouts, setPayouts] = useState([])
+
+  useEffect(() => {
+    const fetchPayouts = async () => {
+      const payouts = await axios("http://localhost:8080/api/payouts")
+      setPayouts(payouts.data)
+    }
+
+    fetchPayouts()
+  }, []);
+
+  return (
+    <Card.Group itemsPerRow={5}>
+      {payouts.map((payout) => (
+        <Card>
+          <Image
+            src={`/images/Artboards_Diversity_Avatars_by_Netguru-${payout.image}.png`}
+            wrapped
+            ui={false}
+          />
+          <Card.Content>
+            <Card.Header>{payout.MERCHANT_ID}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="dollar" />
+              {parseFloat(payout.amount).toFixed(2)}
+            </a>
+          </Card.Content>
+        </Card>
+      ))}
+    </Card.Group>
+  );
+}
+
+export default App;
